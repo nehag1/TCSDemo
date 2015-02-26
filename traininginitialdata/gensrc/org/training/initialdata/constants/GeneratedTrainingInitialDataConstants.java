9812e43b5614efe5082d946fe6febb8a4f5ca485
131005 <dbhargava@ezibuy.co.nz>
2015-02-26 12:49:20 +0530

    committed

---
+++ traininginitialdata/gensrc/org/training/initialdata/constants/GeneratedTrainingInitialDataConstants.java
@@ -0,0 +1,24 @@
+/*
+ * ----------------------------------------------------------------
+ * --- WARNING: THIS FILE IS GENERATED AND WILL BE OVERWRITTEN! ---
+ * --- Generated at 25 Feb, 2015 4:56:20 PM                     ---
+ * ----------------------------------------------------------------
+ */
+package org.training.initialdata.constants;
+
+/**
+ * @deprecated use constants in Model classes instead
+ */
+@Deprecated
+@SuppressWarnings({"unused","cast","PMD"})
+public class GeneratedTrainingInitialDataConstants
+{
+	public static final String EXTENSIONNAME = "traininginitialdata";
+	
+	protected GeneratedTrainingInitialDataConstants()
+	{
+		// private constructor
+	}
+	
+	
+}
